9fd6f86b883c0e01be2e3bb7d4996a359ddbf217
Thauan Ares <thauanares14@gmail.com>
2025-11-13 09:52:55 -0300

    Create SnakeCase.py

---
+++ Exercícios/SnakeCase.py
@@ -0,0 +1,14 @@
+#Convert a PascalCase or CamelCase string to snake case.
+
+def convert_to_snake_case(pascal_or_camel_cased_string):
+
+    snake_cased_char_list = [
+        '_' + char.lower() if char.isupper()
+        else char
+        for char in pascal_or_camel_cased_string
+    ]
+
+    return ''.join(snake_cased_char_list).strip('_')
+
+def main():
+    print(convert_to_snake_case('IAmAPascalCasedString'))
